b237be7f89919257ac9469daf50dbd18fa9ee2da
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-24 20:53:35 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -4,14 +4,60 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const myFirstElement = (
+  <span>
+    <h1>Hello World</h1>
+  </span>
+); // Javascript XML or JSX format
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// React Components
+// Functional Component
+function Hello1() {
+  return <h1><i>Hello Namwan</i></h1>
+}
+
+// Class Component (can have implement logic and state while functional component cannot)
+class Hello2 extends React.Component {
+  render() {
+    return <h1><i>Hello Tin</i></h1>
+  }
+}
+
+// React Props
+function Hello3(props) {
+  return <h1>Hello {props.name}</h1>
+}
+
+class Hello4 extends React.Component {
+  render() {
+    return <h1>Hello {this.props.name}</h1>
+  }
+}
+
+// Component inside Component
+function Someone() {
+  const everyone = ['Tin', 'Namwan', 'Tinwan']
+  var anyone = everyone[Math.floor(Math.random()*everyone.length)]
+  return <span>and {anyone}</span>
+}
+
+class Hello5 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.props.name} <Someone/></h1>
+      </div>
+    )
+  }
+}
+
+// React State
+// State Object
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(myFirstElement)
+root.render(<Hello1/>)
+root.render(<Hello2/>)
+root.render(<Hello3 name="Tinwan"/>)
+root.render(<Hello4 name="Wantin"/>)
+root.render(<Hello5 name="Jubjib"/>)
